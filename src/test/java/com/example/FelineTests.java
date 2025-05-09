8cda71c61102ef23de8ed3bb9eb6d164053688c1
Bulat Maylunov <nunuki3002@gmail.com>
2025-05-09 20:30:48 +0300

    Тесты

--- src/test/java/com/example/FelineTests.java
+++ src/test/java/com/example/FelineTests.java
@@ -1,35 +1,34 @@
-package com.example;
+package com.example; //
 
-import org.junit.jupiter.api.Test;
-import java.util.List;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test; //
+import java.util.List; //
+import static org.junit.jupiter.api.Assertions.*; //
 
-public class FelineTests {
-    @Test
-    void eatMeatReturnsPredatorFoodList() throws Exception {
-        Feline feline = new Feline();
-        List<String> food = feline.eatMeat();
-        assertNotNull(food);
-        assertTrue(food.contains("Животные") || food.contains("Птицы") || food.contains("Рыба"));
-    }
+public class FelineTests { //
+    @Test //
+    void eatMeatReturnsPredatorFoodList() throws Exception { //
+        Feline feline = new Feline(); //
+        List<String> food = feline.eatMeat(); //
+        assertNotNull(food); //
+        assertTrue(food.contains("Животные") || food.contains("Птицы") || food.contains("Рыба")); //
+    } //
 
-    @Test
-    void getFamilyReturnsCats() {
-        Feline feline = new Feline();
-        assertEquals("Кошачьи", feline.getFamily());
-    }
+    @Test //
+    void getFamilyReturnsCats() { //
+        Feline feline = new Feline(); //
+        assertEquals("Кошачьи", feline.getFamily()); //
+    } //
 
-    @Test
-    void getKittensReturnsDefault() {
-        Feline feline = new Feline();
-        assertEquals(1, feline.getKittens());
-    }
+    @Test //
+    void getKittensReturnsDefault() { //
+        Feline feline = new Feline(); //
+        assertEquals(1, feline.getKittens()); //
+    } //
 
-    @Test
-    void getKittensWithParamReturnsCorrectValue() {
-        Feline feline = new Feline();
-        assertEquals(5, feline.getKittens(5));
-    }
-
-
+    @Test //
+    void getKittensWithParamReturnsCorrectValue() { //
+        Feline feline = new Feline(); //
+        assertEquals(5, feline.getKittens(5)); //
+    } //
 }
+
