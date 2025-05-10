a348c566f051166c628314345101d45c1596a1fa
Bulat Maylunov <nunuki3002@gmail.com>
2025-05-10 10:54:05 +0300

    добавлены файлы

--- src/main/java/com/example/Lion.java
+++ src/main/java/com/example/Lion.java
@@ -1,31 +1,31 @@
-package com.example;
+package com.example; //.
 
-import java.util.List;
+import java.util.List; //.
 
-public class Lion {
+public class Lion { //.
 
-    boolean hasMane;
-    private final Feline feline;
-    public Lion(String sex, Feline feline) throws Exception {
-        if ("Самец".equals(sex)) {
-            hasMane = true;
-        } else if ("Самка".equals(sex)) {
-            hasMane = false;
-        } else {
-            throw new Exception("Используйте допустимые значения пола животного - самец или самка");
-        }
-        this.feline = feline;
-    }
+    boolean hasMane; //.
+    private final Feline feline; //.
+    public Lion(String sex, Feline feline) throws Exception { //.
+        if ("Самец".equals(sex)) { //.
+            hasMane = true; //.
+        } else if ("Самка".equals(sex)) { //.
+            hasMane = false; //.
+        } else { //.
+            throw new Exception("Используйте допустимые значения пола животного - самец или самка"); //.
+        } //.
+        this.feline = feline; //.
+    } //.
 
-    public int getKittens() {
-        return feline.getKittens();
-    }
+    public int getKittens() { //.
+        return feline.getKittens(); //.
+    } //.
 
-    public boolean doesHaveMane() {
-        return hasMane;
-    }
+    public boolean doesHaveMane() { //.
+        return hasMane; //.
+    } //.
 
-    public List<String> getFood() throws Exception {
-        return feline.getFood("Хищник");
-    }
-}
+    public List<String> getFood() throws Exception { //.
+        return feline.getFood("Хищник"); //.
+    } //.
+} //.
